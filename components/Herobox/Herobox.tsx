cd1521203b065d384d036b698a32c9b28dcc09c2
gaurav-verma01 <gaurav.verma1@appyhigh.com>
2023-07-06 22:33:16 +0530

    style: herobox

--- components/Herobox/Herobox.tsx
+++ components/Herobox/Herobox.tsx
@@ -2,16 +2,18 @@ import Image from 'next/image';
 import classes from './Herobox.module.css';
 const Herobox = () => {
   return (
-    <section>
+    <section className={classes.section} >
       <div className={classes.content}>
-        <h1 className={classes.heading}>Sharing knowledge, Shaping futures</h1>
+        <h1 className={classes.heading}>
+          Sharing<span className="gradient"> knowledge</span>, Shaping <span className="gradient">futures</span>
+        </h1>
         <p className={classes.subtext}>
           Ignite the potential within every young mind as we inspire, guide, and support them on their journey towards
           personal and academic excellence.
         </p>
       </div>
       <div className={classes.display}>
-        <Image src= "/assets/herobox.svg" alt="herobox" layout="fill" />
+        <Image src="/assets/herobox.svg" alt="herobox" layout="fill" />
       </div>
     </section>
   );
