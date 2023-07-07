fce663d9681805c2be96826d193539e049f78e47
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2023-07-07 21:03:13 +0530

    navbar added

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,38 @@
+import classes from "./Header.module.css"
+import { useState } from "react";
+import clsx from "clsx";
+
 const Header = () => {
-  return <div className="card-link h1">Header</div>;
+
+const [toggle , setToogle] = useState(false);
+
+
+
+
+  return (
+    <nav className={classes.nav}>
+    <div className={classes.logo}>
+     <img src="" alt="Logo Image" />
+    </div>
+    <div className={classes.hamburger} onClick= {()=>{
+      setToogle(!toggle);
+    }} >
+        <div className={classes.line1}></div>
+        <div className={classes.line2}></div>
+        <div className={classes.line3}></div>
+    </div>
+    <ul className={clsx(classes.navlinks, toggle?classes.open:'')}>
+        <li className={clsx( toggle?classes.fade:'')}><a href="/">Home</a></li>
+        <li className={clsx( toggle?classes.fade:'')}><a href="#">About</a></li>
+        <li className={clsx( toggle?classes.fade:'')}><a href="/programs">Program</a></li>
+        <li className={clsx( toggle?classes.fade:'')}><a href="/mentors">Mentors</a></li>
+        {/* <li className={clsx( toggle?classes.fade:'')}><a href="/teams">Teams</a></li>
+        <li className={clsx( toggle?classes.fade:'')}><a href="/startups">Startup</a></li>
+        <li className={clsx( toggle?classes.fade:'')}><a href="/gallery">Gallery</a></li>
+        <li className={clsx( toggle?classes.fade:'')}><a href="/">Contact Us</a></li> */}
+    </ul>
+</nav>
+  );
 };
 
 export default Header;
