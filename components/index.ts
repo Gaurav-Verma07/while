e1075d0f90101b6d66a9ed39e960176e38434ac5
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2023-07-07 15:17:13 +0530

    About section

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,7 @@
 import Layout from './Layout';
 import Herobox from './Herobox';
 import Stats from './Stats';
+import Goals from './Goals';
+import About from './About';
 
-export { Layout, Herobox, Stats };
+export { Layout, Herobox, Stats , Goals , About};
