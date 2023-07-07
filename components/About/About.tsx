05ad7c5c68c4a215d4ba0aae074236c1d868b261
gaurav-verma01 <gaurav.verma1@appyhigh.com>
2023-07-07 19:03:13 +0530

    style: about

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,12 +1,22 @@
-import clsx from "clsx";
-import classes from "./About.module.css"
+import clsx from 'clsx';
+import classes from './About.module.css';
 
 const About = () => {
-    return(
+  return (
     <section className={classes.section}>
-        <h1 className={classes.heading}><center>About<span className="gradient"> Us</span></center></h1>
-        <div className={classes.subtext}>We are dedicated to creating a vibrant community of lifelong learners who are passionate about sharing and expanding their knowledge.Our platform offers a wealth of resources, interactive forums, and collaborative opportunities to  spark intellectual curiosity and foster meaningful connections. Join us on this exciting journey of learning, discovery, and social engagement.</div>
-    </section>);
+      <h1 className={classes.heading}>
+        <center>
+          About<span className="gradient"> Us</span>
+        </center>
+      </h1>
+      <div className={classes.subtext}>
+        We are dedicated to creating a <b>vibrant community</b> of lifelong learners who are passionate about sharing
+        and expanding their knowledge.Our platform offers a wealth of resources, interactive forums, and collaborative
+        opportunities to spark<b> intellectual curiosity</b> and <b>foster meaningful connections</b>. Join us on this
+        exciting journey of learning, discovery, and social engagement.
+      </div>
+    </section>
+  );
 };
 
 export default About;
