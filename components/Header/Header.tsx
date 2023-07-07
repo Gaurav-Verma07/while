4b75648d940bee9f6040914da28759aec4f243d8
gaurav-verma01 <gaurav.verma1@appyhigh.com>
2023-07-07 21:39:36 +0530

    fix: header error

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,33 +1,41 @@
-import classes from "./Header.module.css"
-import { useState } from "react";
-import clsx from "clsx";
+import classes from './Header.module.css';
+import { useState } from 'react';
+import clsx from 'clsx';
+import Link from 'next/link';
 
 const Header = () => {
-
-const [toggle , setToogle] = useState(false);
-
-
-
+  const [toggle, setToogle] = useState(false);
 
   return (
     <nav className={classes.nav}>
-    <div className={classes.logo}>
-     <img src="" alt="Logo Image" />
-    </div>
-    <div className={classes.hamburger} onClick= {()=>{
-      setToogle(!toggle);
-    }} >
+      <div className={classes.logo}>
+        <img src="" alt="Logo Image" />
+      </div>
+      <div
+        className={classes.hamburger}
+        onClick={() => {
+          setToogle(!toggle);
+        }}
+      >
         <div className={classes.line1}></div>
         <div className={classes.line2}></div>
         <div className={classes.line3}></div>
-    </div>
-    <ul className={clsx(classes.navlinks, toggle?classes.open:'')}>
-        <li className={clsx( toggle?classes.fade:'')}><a href="/">Home</a></li>
-        <li className={clsx( toggle?classes.fade:'')}><a href="#">About</a></li>
-        <li className={clsx( toggle?classes.fade:'')}><a href="/programs">Program</a></li>
-        <li className={clsx( toggle?classes.fade:'')}><a href="/mentors">Mentors</a></li>
-    </ul>
-</nav>
+      </div>
+      <ul className={clsx(classes.navlinks, toggle ? classes.open : '')}>
+        <li className={clsx(toggle ? classes.fade : '')}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={clsx(toggle ? classes.fade : '')}>
+          <Link href="#about">About</Link>
+        </li>
+        <li className={clsx(toggle ? classes.fade : '')}>
+          <Link href="#feature">Feature</Link>
+        </li>
+        <li className={clsx(toggle ? classes.fade : '')}>
+          <Link href="/goals">Goals</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
