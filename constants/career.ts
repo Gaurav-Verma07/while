04308fc4739a8f612325db42a4cddcba51282047
vermaanurag1532 <vermaanurag1532@gmail.com>
2023-07-10 01:36:22 +0530

    footer added

--- constants/career.ts
+++ constants/career.ts
@@ -3,18 +3,18 @@ export const data = [
     designation: 'Campus Ambassador',
     about: "Act as a liaison between the organization and the student community, helping to create awareness and generate interest among students about the company.",
     tags: ['Promotion', 'Coordination', 'Outreach'],
-    link: '',
+    link: 'https://forms.gle/hxU3cJGEa67yiFnw7',
   },
   {
     designation: 'Mentee',
     about:  "We are looking for individuals who seeks guidance, support, and mentorship from a more experienced person in a specific field or area of interest.",
     tags: ['Learn', 'Growth', 'Guidence', 'Networking'],
-    link: '',
+    link: 'https://forms.gle/hxU3cJGEa67yiFnw7',
   },
   {
     designation: 'Mentor',
     about: "Join While to provide guidance, support, and expertise to a less experienced person (mentee) in a specific field or area of interest",
     tags: ['Support', 'Experience', 'Development'],
-    link: '',
+    link: 'https://forms.gle/hxU3cJGEa67yiFnw7',
   },
 ];
